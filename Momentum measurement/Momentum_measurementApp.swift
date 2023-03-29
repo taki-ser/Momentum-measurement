95832bc4b1a3e0836bb01f5eff7058ccad2c1ccb
taki-ser <rt12201311@gmail.com>
2023-03-29 12:55:50 +0900

    Initial Commit

---
+++ Momentum measurement/Momentum_measurementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Momentum_measurementApp.swift
+//  Momentum measurement
+//
+//  Created by 滝瀬隆斗 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct Momentum_measurementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Momentum measurementUITests/Momentum_measurementUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Momentum_measurementUITestsLaunchTests.swift
+//  Momentum measurementUITests
+//
+//  Created by 滝瀬隆斗 on 2023/03/29.
+//
+
+import XCTest
+
+final class Momentum_measurementUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
