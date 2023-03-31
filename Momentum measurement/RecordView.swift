f2a8d61679a48137c30cf69b363080ab8f30b4cf
taki-ser <rt12201311@gmail.com>
2023-03-31 13:57:40 +0900

    delete機能の実装中。GPTと前のコードのコメントアウト。

--- Momentum measurement/RecordView.swift
+++ Momentum measurement/RecordView.swift
@@ -29,7 +29,9 @@ struct RecordView: View {
     @State private var activityFolderName: String = ""
     @State var listOfPath: [URL] = []
     @State private var showingAddFolderAlart = false
-    
+    @State var indexOfDeleteItem: Int = 0
+    @State private var showingDeleteAlert = false
+    @State private var deleteCandidate: URL?
     var body: some View {
                 NavigationView {
 //                    List(records) { record in
@@ -38,6 +40,37 @@ struct RecordView: View {
                         ForEach(listOfPath, id: \.self) { url in
                             ItemCell(url:url)
                         }
+//                        .onDelete { (offsets) in
+//                            if let index: Int = offsets.first {
+//                                indexOfDeleteItem = index
+//                                self.showingDeleteAlert = true
+//                            }
+//                        }
+                        .onDelete { (indexSet) in
+                                            if let index = indexSet.first {
+                                                deleteCandidate = listOfPath[index]
+                                                showingDeleteAlert = true
+                                            }
+                                        }
+//                        .alert(isPresented: $showingDeleteAlert) {
+//                            Alert(title: Text("警告"),
+//                                  message: Text("\"\(listOfPath[indexOfDeleteItem].lastPathComponent    )\"は削除されます。"),
+//                                  primaryButton: .cancel(Text("キャンセル")),    // キャンセル用
+//                                  secondaryButton: .destructive(Text("削除"), action: {removeItem(atPath: listOfPath[indexOfDeleteItem])})
+//                            )   // 破壊的変更用
+//                        }
+                        .alert(isPresented: $showingDeleteAlert) {
+                            Alert(title: Text("削除の確認"),
+                                  message: Text("「\(deleteCandidate?.lastPathComponent ?? "")」を削除しますか？"),
+                                  primaryButton: .cancel(Text("キャンセル")),
+                                  secondaryButton: .destructive(Text("削除"), action: {
+                                    if let candidate = deleteCandidate {
+                                        removeItem(atPath: candidate)
+                                        deleteCandidate = nil
+                                    }
+                                  }))
+                        }
+                        
                     }
                     .navigationTitle("Activity")
                     .toolbar {
@@ -70,52 +103,7 @@ struct RecordView: View {
                        })
             }
 
-//        List(records) { record in
-//                        Text("\(record.date): \(record.filename)")
-//                    }
-//                    .navigationTitle("Records")
-//                    .toolbar {
-//                        ToolbarItem(placement: .navigationBarTrailing) {
-//                            Button(action: {
-////                                saveRecord()
-//                            }, label: {
-//                                Image(systemName: "plus")
-//                            })
-//                        }
-//                    }
-////        NavigationView {
-//            List{
-//                ForEach(listOfPath, id: \.self) { url in
-//                    ItemCell(url:url)
-//                }
-//            }
-////            .toolbar {
-////                ToolbarItemGroup(placement: .navigationBarTrailing) {
-////                    Button(action: {createDirectory()},
-////                           label: {
-////                        Image(systemName: "folder.badge.plus")
-////                    })
-////                }
-//            }
-////            .navigationTitle("Test")
-////            .navigationBarTitleDisplayMode(.automatic)
-////        }
-    func getFolder(url: URL) -> [URL] {
-        do {
-            /// フォルダのURLからふくまれるURLを取得
-            ///  let keys = [URLResourceKey.contentModificationDateKey]
-            var fileAndFolderURLs = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
-     
-            /// ファイル名でソート
-            fileAndFolderURLs.sort(by: {$0.lastPathComponent < $1.lastPathComponent})
-    //        print(fileAndFolderURLs)
-            return fileAndFolderURLs
-        } catch {
-            print(error)
-            return []
-        }
-    }
-    
+        
     func createDirectory() {
         guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
             return
@@ -130,6 +118,17 @@ struct RecordView: View {
             print(error.localizedDescription)
         }
     }
+    
+    func removeItem(atPath path: URL) {
+        let fileManager = FileManager.default
+        do {
+            try fileManager.removeItem(at: path)
+            listOfPath = getFolder(url: URL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]))
+        } catch {
+            fatalError("Failed to remove item at path \(path). Error: \(error.localizedDescription)")
+        }
+    }
+    
 
     func saveRecord(directory: URL) {
         let date = Date()
@@ -183,134 +182,25 @@ struct SubFolder: View {
         print("\(path)はデリートされた\n")
         self.showingDeleteAlert = false
     }
-    func getFolder(url: URL) -> [URL] {
-        do {
-            /// フォルダのURLからふくまれるURLを取得
-            ///  let keys = [URLResourceKey.contentModificationDateKey]
-            var fileAndFolderURLs = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
-     
-            /// ファイル名でソート
-            fileAndFolderURLs.sort(by: {$0.lastPathComponent < $1.lastPathComponent})
-    //        print(fileAndFolderURLs)
-            return fileAndFolderURLs
-        } catch {
-            print(error)
-            return []
-        }
-    }
 }
 
 
+func getFolder(url: URL) -> [URL] {
+    do {
+        /// フォルダのURLからふくまれるURLを取得
+        ///  let keys = [URLResourceKey.contentModificationDateKey]
+        var fileAndFolderURLs = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil)
+ 
+        /// ファイル名でソート
+        fileAndFolderURLs.sort(by: {$0.lastPathComponent < $1.lastPathComponent})
+//        print(fileAndFolderURLs)
+        return fileAndFolderURLs
+    } catch {
+        print(error)
+        return []
+    }
+}
 
-
-//struct RecordView: View {
-//    @State private var records: [Record] = []
-//    @State private var directoryName: String = ""
-//
-//    var body: some View {
-//        NavigationView {
-//            List(records) { record in
-//                Text("\(record.date): \(record.filename)")
-//            }
-//            .navigationTitle("Records")
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    Button(action: {
-//                        createDirectory()
-//                    }, label: {
-//                        Image(systemName: "plus")
-//                    })
-//
-//                }
-//            }
-//        }
-//    }
-//
-//    func createDirectory() {
-//        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-//            return
-//        }
-//        let directoryURL = documentsDirectory.appendingPathComponent(directoryName, isDirectory: true)
-//
-//        do {
-//            try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
-//            saveRecord(directory: directoryURL)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//
-//    func saveRecord(directory: URL) {
-//        let date = Date()
-//        let filename = "record_\(date.timeIntervalSince1970).csv"
-//        let fileURL = directory.appendingPathComponent(filename)
-//        do {
-//            try "".write(to: fileURL, atomically: true, encoding: .utf8)
-//            records.append(Record(date: date, filename: filename))
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//}
-
-
-//struct RecordView: View {
-//    @State private var records: [Record] = []
-//    @State private var directoryName: String = ""
-//    @State private var isShowingAlert = false
-//
-//    var body: some View {
-//        NavigationView {
-//            List(records) { record in
-//                Text("\(record.date): \(record.filename)")
-//            }
-//            .navigationTitle("Records")
-//            .toolbar {
-//                ToolbarItem(placement: .navigationBarTrailing) {
-//                    Button(action: {
-//                        isShowingAlert = true
-//                    }, label: {
-//                        Image(systemName: "plus")
-//                    })
-//                }
-//            }
-//            .alert(isPresented: $isShowingAlert, content: {
-//                Alert(title: Text("Enter Directory Name"),
-//                      message: Text("Please enter a name for the directory."),
-//                      primaryButton: .default(Text("Save"), action: {
-//                          createDirectory()
-//                      }),
-//                      secondaryButton: .cancel())
-//            })
-//        }
-//    }
-//
-//    func createDirectory() {
-//        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-//            return
-//        }
-//        let directoryURL = documentsDirectory.appendingPathComponent(directoryName, isDirectory: true)
-//
-//        do {
-//            try FileManager.default.createDirectory(at: directoryURL, withIntermediateDirectories: true, attributes: nil)
-//            saveRecord(directory: directoryURL)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//
-//    func saveRecord(directory: URL) {
-//        let date = Date()
-//        let filename = "record_\(date.timeIntervalSince1970).csv"
-//        let fileURL = directory.appendingPathComponent(filename)
-//        do {
-//            try "".write(to: fileURL, atomically: true, encoding: .utf8)
-//            records.append(Record(date: date, filename: filename))
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-//}
 
 
 
